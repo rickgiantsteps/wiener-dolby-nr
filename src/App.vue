9ba251ac2479edcd52b6b5c9c22e72722efcd8f1
Riccardo Passoni <riccardopassoni@protonmail.com>
2023-10-24 15:10:51 +0200

    noise gen layout

--- src/App.vue
+++ src/App.vue
@@ -113,13 +113,13 @@
 
         <div class="effects-choice-container dark:bg-slate-600 shadow shadow-[#6da4ba] dark:shadow-white">
 
-          <label class="text-2xl font-bold flex justify-center text-[#6da4ba] dark:text-white">Noise generator</label>
-          <section class="synth-type flex-1">
-            <button class="justify-center text-white font-bold py-2 px-4 rounded-full bg-[#6da4ba] shadow-sm shadow-[#6da4ba] dark:shadow-[#6da4ba] hover:bg-[#3e6f83]" @click="playNoise(noise)">Play noise</button>
-            <button class="justify-center text-white font-bold py-2 px-4 rounded-full bg-[#6da4ba] shadow-sm shadow-[#6da4ba] dark:shadow-[#6da4ba] hover:bg-[#3e6f83]" @click="stopNoise(noise)">Stop</button>
-            <div class="synth-selections shadow shadow-[#6da4ba] dark:shadow-white">
+          <label class="text-2xl font-bold flex justify-center text-[#6da4ba] dark:text-white mb-2">Noise generator</label>
+          <section class="synth-type">
+            <button class="mb-3 justify-center text-white font-bold py-2 px-4 rounded-full bg-[#6da4ba] shadow-sm shadow-[#6da4ba] dark:shadow-[#6da4ba] hover:bg-[#3e6f83]" @click="playNoise(noise)">Play noise</button>
+            <button class="mb-3 justify-center text-white font-bold py-2 px-4 rounded-full bg-[#6da4ba] shadow-sm shadow-[#6da4ba] dark:shadow-[#6da4ba] hover:bg-[#3e6f83]" @click="stopNoise(noise)">Stop</button>
+            <div class="synth-selections mr-3 shadow shadow-[#6da4ba] dark:shadow-white">
               <label class="text-base font-semibold text-[#6da4ba] dark:text-white ">White Noise</label>
-              <div class="synth-slider-container dark:text-white">
+              <div class="noise-slider-container dark:text-white">
                 <!--<section>
                   <form>
                     <button type="button" class="button-81" name="triangle0">Triangle</button>
@@ -128,6 +128,8 @@
                     <button type="button" class="button-80" name="sawtooth0">Saw</button>
                   </form>
                 </section>-->
+                <section>
+                </section>
                 <section>
                   <input type="range"
                          min="0"
